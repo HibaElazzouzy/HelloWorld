54bbb101fe5b13f6ab421ca1135a0ae5d5ff1cd8
HibaElazzouzy <148236721+HibaElazzouzy@users.noreply.github.com>
2023-10-19 00:00:41 +0100

    Ajoutez des fichiers projet.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Programme
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
